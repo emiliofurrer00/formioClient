3008183fd42a131c4532fa114d6d9687e678941f
Emilio F <emiliofurrer00@gmail.com>
2025-12-20 18:12:07 +0100

    add missing slash to url

--- app/forms/[formGuid]/page.tsx
+++ app/forms/[formGuid]/page.tsx
@@ -6,7 +6,7 @@ export default async function FormPage({
     params: Promise<{ formGuid: string }>;
 }) {
     const { formGuid } = await params;
-    const formData = await fetch(process.env.API_URL + 'forms/' + formGuid).then(res => res.json());
+    const formData = await fetch(process.env.API_URL + '/forms/' + formGuid).then(res => res.json());
     console.log(formData);
 
     return (
